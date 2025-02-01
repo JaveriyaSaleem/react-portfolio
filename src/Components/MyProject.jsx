92b0215376b8f6bdf72bff54ad6fa862bc4d5f24
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2025-02-01 19:43:34 +0500

    added

--- src/Components/MyProject.jsx
+++ src/Components/MyProject.jsx
@@ -5,17 +5,17 @@ import Card3 from "../assets/img/Card Project 3.png"
 
 const MyProject = () => {
   return (
-    <div className='px-10 pt-10 pb-20 '>
+    <div className='px-20 pt-10 pb-20 '>
       <h1 className='text-[45px] text-center'>My Project</h1>
-      <div className='flex justify-center items-center'>
-        <div>
+      <div className='flex justify-center items-center gap-3'>
+        <div className='cursor-pointer'>
         <img src={Card1} alt="img"  />
         </div>
-        <div>
+        <div className='cursor-pointer'>
        
         <img src={Card2} alt="img"  />
         </div>
-        <div>
+        <div className='cursor-pointer'>
         <img src={Card3} alt="img" />
         </div>
       </div>
